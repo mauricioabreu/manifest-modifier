c09fc61b02b8410d5f5e9a42bcdc24bd27ef0c35
Maurício Antunes <mauricio.abreua@gmail.com>
2022-09-29 22:30:08 -0300

    Filter by bandwidth

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,15 @@ fn filter_fps(pl: MasterPlaylist, rate: f64) -> MasterPlaylist {
     mpl
 }
 
+fn filter_bandwidth(pl: MasterPlaylist, min: u64, max: u64) -> MasterPlaylist {
+    let mut mpl = pl.clone();
+    mpl.variants = pl.variants
+        .into_iter()
+        .filter( |v| v.bandwidth >= min)
+        .collect::<Vec<m3u8_rs::VariantStream>>();
+    mpl
+}
+
 #[cfg(test)]
 mod tests {
     use std::io::Read;
@@ -24,5 +33,17 @@ mod tests {
             let nmp = filter_fps(master_playlist, 60.0);
             assert_eq!(nmp.variants.len(), 2);
         };
+    }
+
+    #[test]
+    fn filter_min_bandwidth() {
+        let mut file = std::fs::File::open("manifests/master.m3u8").unwrap();
+        let mut content: Vec<u8> = Vec::new();
+        file.read_to_end(&mut content).unwrap();
+
+        if let Result::Ok((_, master_playlist)) =  m3u8_rs::parse_master_playlist(&content) {
+            let nmp = filter_bandwidth(master_playlist, 800000, 0);
+            assert_eq!(nmp.variants.len(), 3);
+        }
     }
 }
