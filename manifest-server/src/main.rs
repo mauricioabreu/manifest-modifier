26e36c58e3fb0b321167b6c77c2ef12044e15173
Maurício Antunes <mauricio.abreua@gmail.com>
2022-10-01 16:02:50 -0300

    Simple route to test axum

--- manifest-server/src/main.rs
+++ manifest-server/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{post},
+    http::StatusCode,
+    response::IntoResponse,
+    Router,
+};
+use tracing_subscriber;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", post(modify_manifest));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn modify_manifest() -> &'static str {
+    "Hello, World!"
 }
